c597400840f1195f8eb024c37906b965531ddeb6
bigrushdog <randall.rushing@gmail.com>
2017-08-16 13:56:20 -0700

    DUI: Migrate to API/Permission model for Editor [3/5]
    Change-Id: Ib6ca4e745f1aec383c3e260aac482de5d393ee5a

--- src/com/android/systemui/navigation/smartbar/SmartBarView.java
+++ src/com/android/systemui/navigation/smartbar/SmartBarView.java
@@ -59,6 +59,7 @@ import com.android.internal.utils.du.Config.ActionConfig;
 import com.android.internal.utils.du.Config.ButtonConfig;
 import com.android.systemui.navigation.BaseEditor;
 import com.android.systemui.navigation.BaseNavigationBar;
+import com.android.systemui.navigation.Editor;
 import com.android.systemui.navigation.OpaLayout;
 import com.android.systemui.navigation.Res;
 import com.android.systemui.navigation.NavigationController.NavbarOverlayResources;
@@ -230,6 +231,11 @@ public class SmartBarView extends BaseNavigationBar {
 
     float getButtonAlpha() {
         return mCustomAlpha;
+    }
+
+    @Override
+    public Editor getEditor() {
+        return mEditor;
     }
 
     @Override
@@ -569,9 +575,6 @@ public class SmartBarView extends BaseNavigationBar {
 
     @Override
     protected void onDispose() {
-        if (mEditor != null) {
-            mEditor.unregister();
-        }
     }
 
     @Override
